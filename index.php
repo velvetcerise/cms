6d6a9d930d3ac048f743376c616a7b59628da225
Angelina <alicona@bexponencial.com>
2019-10-21 17:52:38 -0500

    Cambia contenido por arrays para pintarlos en el blog

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php 
+
+	// Configura la salida de errores por pantalla
+	error_reporting( E_ALL );
+	ini_set( 'display_errors', 1 );
+
+	// Array con array disociativo
+	$all_posts = [
+		[
+			'id' => 1,
+			'title' => 'Lorem ipsum dolor sit amet',
+			'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae pulvinar turpis',
+			'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae pulvinar turpis. Nam ut arcu tellus. Morbi sit amet elit lacinia, tincidunt leo a, posuere mi. Mauris nec odio at quam lacinia consequat. Fusce mattis orci ex, eget accumsan neque vehicula et. Vivamus consectetur tempor lacus, in tincidunt massa rutrum ut. Pellentesque augue felis, iaculis eu interdum et, semper eu purus. Vestibulum a viverra justo.',
+			'published_on' => '2018-01-11 10:15:00',
+		],
+		[
+			'id' => 2,
+			'title' => 'Nunc eget enim vulputate',
+			'excerpt' => 'Integer placerat hendrerit pharetra. Nunc eget enim vulputate, efficitur dolor pretium',
+			'content' => 'Integer placerat hendrerit pharetra. Nunc eget enim vulputate, efficitur dolor pretium, pharetra nulla. Proin mattis aliquam sem. Morbi vel mi ac magna consequat tempus vitae eget diam. Aliquam ac sapien a tortor rutrum faucibus nec nec urna. Ut et nisl magna. Vivamus elit risus, rhoncus vitae elit suscipit, porta pulvinar justo. Aliquam sodales urna eu scelerisque ultrices. Fusce et neque id risus gravida vestibulum a et urna. Curabitur aliquam accumsan leo, pharetra tempus velit condimentum et. Donec dapibus faucibus lorem a tincidunt. Donec ultricies id metus et aliquam. Vestibulum dapibus magna nec elit ultrices, ornare pretium nisi dictum.',
+			'published_on' => '2018-01-11 10:15:00',
+		],
+	];
+
 	// Funciones
 
 	function get_post_1_titulo() {
@@ -21,60 +44,6 @@
 		return $post_2_contenido;
 	}
 
-	// Tipos de datos
-
-	// Integers
-	$positivo = 10;
-	$cero = 0;
-	$negativo = -10;
-
-	// Punto flotante (reales) floats, doubles
-	$numero = 1.234;
-	$exponente_1 = 0.1234e2; // 0.1234 * 10 ^ 2
-	$exponente_2 = 1234E-3; // 1234 * 10^ - 3
-
-
-	// Cadenas: Strings
-	$nombre = 'Ignacio'; // Comillas Simples
-	$apellido = "Cruz"; // Comillas Dobless
-
-
-	// Array
-
-	// Notación antigua
-	$alimento = array( 'pera', 'manzana', 'carne');
-	$colores = [ 'azul', 'verde', 'rojo']; 
-
-	// Acceder arrays
-	var_dump( $colores[0] );
-	var_dump( $colores[1] );
-
-
-	// Array Asociativo
-	$edades	= ['Pedro' => 22, 'Ana' => 15, 'Susana' => 38 ];
-	var_dump( $edades['Pedro']);
-	var_dump( $edades['Susana']);
-
-	// Añadir elementos a arrays
-	$colores[] = 'morado';
-	$edades['Juan'] = 45;
-	
-	var_dump($edades);
-	var_dump($colores);
-
-
-	// Valores "false"
-	$cero = 0;
-	$cadena_vacia = '';
-	$nulo = null;
-	$array_vacio = [];
-
-	// Valores "true"
-	// (bool) => Convierte a booleno
-	var_dump( (bool) $positivo );
-	var_dump( (bool) $negativo );
-	var_dump( (bool) $edades );
-	var_dump( (bool) $nombre );
 
 ?>
 
@@ -100,15 +69,29 @@
 
 <div id="content" >
 	<div class="posts">
-		<div>
-			<h2><?php echo get_post_1_titulo(); ?></h2>
-			<div><?php echo get_post_1_contenido(); ?></div>
-		</div>
+		<article class="post">
+			<header>
+				<h2 class="post-title"><?php echo $all_posts[0]['title'];?></h2>
+			</header>
 
-		<div>
-			<h2><?php echo get_post_2_titulo(); ?></h2>
-			<div><?php echo get_post_2_contenido(); ?></div>
-		</div>
+			<div class="post-content">
+				<?php echo $all_posts[0]['content'];?>
+			</div>
+
+			<footer></footer>
+		</article>
+
+		<article class="post">
+			<header>
+				<h2 class="post-title"><?php echo $all_posts[1]['title'];?></h2>
+			</header>
+
+			<div class="post-conten">
+				<?php echo $all_posts[1]['content'];?>
+			</div>
+
+			<footer></footer>
+		</article>
 	</div>
 </div>
 
