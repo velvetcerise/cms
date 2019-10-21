bb5b95c18dbf1fbf36be53814bd9ef31321b7b99
Angelina <alicona@bexponencial.com>
2019-10-21 15:05:26 -0500

    Cambia las variables por funciones, agrega ejemplo para llamar variables globales en una función

--- index.php
+++ index.php
@@ -1,51 +1,46 @@
 <?php 
 
-	$post_1_titulo = 'Lorem ipsum dolor sit amet';
-	$post_1_contenido = 'Mauris lobortis, turpis sit amet pulvinar henderit, elit lingula accusman lingula, ut';
-	$post_2_titulo = 'Mauris lobortis, turpis sit amet pulvinar henderit';
-	$post_2_contenido = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis in odit aperiam eos placeat ullam vero veniam iste aspernatur mollitia rem excepturi, minima consectetur ut sunt maxime natus, enim beatae.Adipisci ea, ex numquam dolorum blanditiis quis iure magni quasi voluptate expedita veritatis quos laborum vel molestias reprehenderit cupiditate obcaecati aliquam eos eum dignissimos consequatur! Odit accusantium suscipit quod ratione.';
+	// Funciones
+	function get_post_1_titulo() {
+		$post_1_titulo = 'Lorem ipsum dolor sit amet';
+		return $post_1_titulo;
+	}
 
-	function nombre_de_funcion($param1, $param2) {
-		// Código a ejecutar
+	function get_post_1_contenido() {
+		$post_1_contenido = 'Mauris lobortis, turpis sit amet pulvinar henderit, elit lingula accusman lingula, ut';
+		return $post_1_contenido;
+	}
+
+	function get_post_2_titulo() {
+		$post_2_titulo = 'Mauris lobortis, turpis sit amet pulvinar henderit';
+		return $post_2_titulo;
+	}
+
+	function get_post_2_contenido() {
+		$post_2_contenido = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis in odit aperiam eos placeat ullam vero veniam iste aspernatur mollitia rem excepturi, minima consectetur ut sunt maxime natus, enim beatae.Adipisci ea, ex numquam dolorum blanditiis quis iure magni quasi voluptate expedita veritatis quos laborum vel molestias reprehenderit cupiditate obcaecati aliquam eos eum dignissimos consequatur! Odit accusantium suscipit quod ratione.';
+		return $post_2_contenido;
 	}
 
 
-	// Funcíon sin parámetro
-	function que_dia_es_hoy() {
-		echo 'hoy es ' . date( 'l' );
+
+	// Ámbito de variables (Scope)
+	function prueba() {
+		$saludo = 'Hola, ¿Qué tal?';
+		echo $saludo;
 	}
 
-	// Función con parámetro
-	function suma3( $num1, $num2, $num3 ) {
-		// echo $num1 + $num2 + $num3;
-		return $num1 + $num2 + $num3;
-
+	// Global
+	$otro_saludo = 'HOLA!';
+	function otra_prueba() {
+		global $otro_saludo; // Llamamos variable global
+		echo $otro_saludo;	// Estamos llamando a la variable, pero como es global, no se imprime
 	}
-
-	function incrementa( $num, $inc = 1) {
-		return $num + $inc;
-	}
+	
 ?>
 
-	<!-- Funciones  -->
-	<!-- Pueden sacar o devolver por pantalla algo -->
-	<!-- Min, devuelve un número; -->
-	<h1><?php echo min(10, 2); ?></h1>
 
-	<!-- Floor redondea números décimales -->
-	<h1><?php echo floor(4/3); ?></h1>
-	
-	<h1><?php echo pi(); ?></h1>
-
-	<!-- Devuelve el día de hoy -->
-	<h1><?php que_dia_es_hoy(); ?></h1>
-
-	<!-- Devuelve la función suma -->
-	<h1><?php echo suma3( 10, 5, 2 ); ?></h1>
-
-	<!-- Devuelve el incremento -->
-	<h1><?php echo incrementa(10, 3); ?></h1>
-	<h1><?php echo incrementa(10); ?></h1> <!-- No tiene incremento -->
+<h1><?php prueba(); ?></h1>
+<h1><?php otra_prueba(); ?></h1>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -70,13 +65,13 @@
 <div id="content" >
 	<div class="posts">
 		<div>
-			<h2><?php echo $post_1_titulo; ?></h2>
-			<div><?php echo $post_1_contenido; ?></div>
+			<h2><?php echo get_post_1_titulo(); ?></h2>
+			<div><?php echo get_post_1_contenido(); ?></div>
 		</div>
 
 		<div>
-			<h2><?php echo $post_1_titulo; ?></h2>
-			<div><?php echo $post_1_contenido; ?></div>
+			<h2><?php echo get_post_2_titulo(); ?></h2>
+			<div><?php echo get_post_2_contenido(); ?></div>
 		</div>
 	</div>
 </div>
