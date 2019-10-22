73b1f7dd83ecaccd4af498290be8a337d01d9c7e
Angelina <alicona@bexponencial.com>
2019-10-22 13:19:57 -0500

    Termina ejercicio para pintar la fecha de publicación del array

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
 			'title' => 'Lorem ipsum dolor sit amet',
 			'excerpt' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae pulvinar turpis',
 			'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae pulvinar turpis. Nam ut arcu tellus. Morbi sit amet elit lacinia, tincidunt leo a, posuere mi. Mauris nec odio at quam lacinia consequat. Fusce mattis orci ex, eget accumsan neque vehicula et. Vivamus consectetur tempor lacus, in tincidunt massa rutrum ut. Pellentesque augue felis, iaculis eu interdum et, semper eu purus. Vestibulum a viverra justo.',
-			'published_on' => '2018-01-11 10:15:00',
+			'published_on' => '2018-02-17 10:15:00',
 		],
 		[
 			'id' => 2,
 			'title' => 'Nunc eget enim vulputate',
 			'excerpt' => 'Integer placerat hendrerit pharetra. Nunc eget enim vulputate, efficitur dolor pretium',
 			'content' => 'Integer placerat hendrerit pharetra. Nunc eget enim vulputate, efficitur dolor pretium, pharetra nulla. Proin mattis aliquam sem. Morbi vel mi ac magna consequat tempus vitae eget diam. Aliquam ac sapien a tortor rutrum faucibus nec nec urna. Ut et nisl magna. Vivamus elit risus, rhoncus vitae elit suscipit, porta pulvinar justo. Aliquam sodales urna eu scelerisque ultrices. Fusce et neque id risus gravida vestibulum a et urna. Curabitur aliquam accumsan leo, pharetra tempus velit condimentum et. Donec dapibus faucibus lorem a tincidunt. Donec ultricies id metus et aliquam. Vestibulum dapibus magna nec elit ultrices, ornare pretium nisi dictum.',
-			'published_on' => '2018-01-11 10:15:00',
+			'published_on' => '2018-01-18 10:15:00',
 		],
 	];
 ?>
@@ -66,12 +66,7 @@
 				<footer>
 					<span class="post-date">
 						Publicada en:
-						<!-- <?php echo date( 'Y, m-d' ) ?> Fecha de hoy -->
-						<!-- <?php echo date( 'Y, m-d H:i:s' ) ?> Fecha de hoy con hora, minutos y segundos  -->
-
-						<?php $timestamp = strtotime ( $post['published_on'] ); ?>
-						<!-- <?php echo date('d M Y', $timestamp ); ?> -->
-						<?php echo strftime( '%d %b %Y', $timestamp ); ?>
+						<?php echo strftime( '%d %b %Y', strtotime ( $post['published_on'] ) ); ?>
 					</span>
 				</footer>
 			</article>
